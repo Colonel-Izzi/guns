bd9a5ccaf8a0f31f7b38a5a518c802cc47200efc
Colonel-Izzi <42562762+Colonel-Izzi@users.noreply.github.com>
2022-10-15 09:28:02 +1100

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -22,9 +22,9 @@ const DEFAULT_GUN_OPTIONS = [
                         resourceId: "artifact:2112691761322131840",
                         transform: {
                                 position: {
-                                        x: 0,
+                                        x: 0.0445,
                                         y: 0,
-                                        z: 0
+                                        z: 0.2255
                                 },
                                 rotation: {
                                         x: 180,
